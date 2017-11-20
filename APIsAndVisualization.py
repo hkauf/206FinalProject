3f071c094acb53aba1d6fec75ebfed3b924b0c5a
hkauf <hkauf@umich.edu>
2017-11-20 11:15:42 -0500

    Started Project

---
+++ APIsAndVisualization.py
@@ -0,0 +1,7 @@
+#Hallie Kaufman 
+#SI 206 
+#Data-Oriented Programming with APIs and Visualization Final Project
+
+#Below are the necessary import statements for the project
+import json
+import pprint
