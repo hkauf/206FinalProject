f903f73d4863e827cbafe713c11a11ab4bdaa48c
hkauf <hkauf@umich.edu>
2017-11-20 15:11:45 -0500

    Cache created

--- APIsAndVisualization.py
+++ APIsAndVisualization.py
@@ -5,3 +5,12 @@
 #Below are the necessary import statements for the project
 import json
 import pprint
+#setting up cache:
+CACHE_FNAME = "APIsAndVisualization_cache.json"
+try:
+    cache_file = open(CACHE_FNAME, 'r')
+    cache_contents = cache_file.read()
+    cache_file.close()
+    CACHE_DICTION = json.loads(cache_contents)
+except:
+    CACHE_DICTION = {}
