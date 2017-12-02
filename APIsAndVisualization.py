55315e101edfeeb1eb6bbb0a0bbe3401a2345310
hkauf <hkauf@umich.edu>
2017-12-02 00:02:25 -0500

    Continued work

--- APIsAndVisualization.py
+++ APIsAndVisualization.py
@@ -3,12 +3,15 @@
 #Data-Oriented Programming with APIs and Visualization Final Project
 
 #Below are the necessary import statements for the project
+import requests
 import json
 import sqlite3
+import datetime
 import pprint
+import Facebook.py
 
 
-api1 = 
+
 #Setting up cache:
 CACHE_FNAME = "APIsAndVisualization_cache.json"
 try:
@@ -18,3 +21,13 @@ try:
     CACHE_DICTION = json.loads(cache_contents)
 except:
     CACHE_DICTION = {}
+
+#Facebook API
+print ("Facebook\n")
+def FB_Time_Frame():
+    days_list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+
+
+fb_access_token = ''
+
