249b1ac23a3a5dc1191ff77f6dcfd47ac926a95e
hkauf <hkauf@umich.edu>
2017-12-04 13:42:27 -0500

    Worked on Facebook days/ times

--- APIsAndVisualization.py
+++ APIsAndVisualization.py
@@ -8,7 +8,7 @@ import json
 import sqlite3
 import datetime
 import pprint
-import Facebook.py
+import Facebook
 
 
 
@@ -26,8 +26,15 @@ except:
 print ("Facebook\n")
 def FB_Time_Frame():
     days_list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    time_list = ['12:00 am - 5:59 am', '6:00 am - 11:59 am', '12:00 pm - 5:59 pm', '6:00 pm - 11:59pm']
+    sort_day_time = []
+    for time in time_list:
+    	for day in days_list:
+    		sort_day_time.append(day + ' ' + time)
+	return (sort_day_time)
+print (sort_day_time)
 
 
 
-fb_access_token = ''
+fb_access_token = Facebook.access_token
 
