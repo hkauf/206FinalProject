dcb9332e058862339f1cbd3515c05679b345810e
hkauf <hkauf@umich.edu>
2017-12-08 20:44:51 -0500

    making likes counter and sql table

--- APIsAndVisualizations.py
+++ APIsAndVisualizations.py
@@ -22,7 +22,7 @@ except:
     CACHE_DICTION = {}
 
 #Facebook API
-print ("Facebook\n")
+print ("Facebook Data Collection\n")
 # def FB_Time_Frame():
 #     days_list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 #     time_list = ['12:00 am - 5:59 am', '6:00 am - 11:59 am', '12:00 pm - 5:59 pm', '6:00 pm - 11:59pm']
@@ -35,7 +35,46 @@ print ("Facebook\n")
 # pass
 
 def get_facebook_data(access_token_in):
-    graph_api = facebook.GraphAPI(access_token = access_token_in, version= '2.1')
-    posts = graph_api.request('me?fields=posts.limit(100)')
-    return posts
-pprint(get_facebook_data(Facebook.access_token))
+    if access_token_in in CACHE_DICTION:
+        print ('using cache...')
+        results = CACHE_DICTION[access_token_in]
+    else:
+        print('fetching data...')
+        results = []
+        graph_api = facebook.GraphAPI(access_token = access_token_in, version= '2.1')
+        pictures = graph_api.request('me?fields=photos.limit(100){created_time,picture,likes{id}}')
+        # posts = graph_api.request('me?fields=posts.limit(100){status_type,created_time,likes{id}}')
+        results.append(pictures)
+        # results.append(posts)
+
+        CACHE_DICTION[access_token_in] = results
+        wfile = open(CACHE_FNAME, 'w')
+        wfile.write(json.dumps(CACHE_DICTION))
+        wfile.close()
+
+    return results
+   
+
+data = pprint(get_facebook_data(Facebook.access_token))
+    
+print('\n LIKES\n')
+
+def get_likes(data):
+    likes = data['photos']['data']['likes']
+    likes_id= likes['data']['id']
+    likescount = 0
+    for numlikes in likes:
+        if likes_id in likes:
+            likescount+=1
+    return likescount
+    
+show_likes = print(get_likes(data))
+
+
+#SQL
+conn = sqlite3.connect('FBData.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Facebook')
+cur.execute('CREATE TABLE Facebook (user_id TEXT, created_at TIMESTAMP, Photos TEXT, Likes INTEGER, Day TEXT)')
+
